dfefceca937003c8ebf0661e5c4108a2046186bc
Jishaal Kalyan <jishaal@gmail.com>
2021-04-15 23:27:41 +1200

    Transfer blog posts

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 import { PageSeo } from '@/components/SEO'
@@ -22,8 +23,8 @@ export default function About() {
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
               {siteMetadata.author}
             </h3>
-            <div className="text-gray-500 dark:text-gray-400">Professor of Atmospheric Science</div>
-            <div className="text-gray-500 dark:text-gray-400">Stanford University</div>
+            <div className="text-gray-500 dark:text-gray-400">Frontend Engineer Lead</div>
+            <div className="text-gray-500 dark:text-gray-400">Parrot Analytics</div>
             <div className="flex pt-6 space-x-3">
               <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
               <SocialIcon kind="github" href={siteMetadata.github} />
@@ -34,20 +35,21 @@ export default function About() {
             </div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
+            <Image
+              src="/static/images/me.jpg"
+              alt="Picture of the author"
+              width={727}
+              height={585}
+            />
             <p>
-              Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His
-              research interests includes complexity modelling of tailwinds, headwinds and
-              crosswinds.
+              I'm a Frontend Engineer with 8+ years in the industry. I work with modern frontend
+              technologies such as React, Redux, Node.js and TypeScript.
             </p>
             <p>
-              He leads the clean energy group which develops 3D air pollution-climate models, writes
-              differential equation solvers, and manufactures titanium plated air ballons. In his
-              free time he bakes raspberry pi.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique
-              placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem
-              nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+              The web has fascinated me since I was a child, having created and deployed my first
+              website in my teens during the early 2000s. Since then, technologies that help power
+              today's websites has changed dramatically allowing us to create seamless experiences
+              and solve problems in ways deemed impossible just a few years ago.
             </p>
           </div>
         </div>
