dac87e7d5f0bc3a6f58e1572620693009624f64c
Jishaal Kalyan <jishaal@gmail.com>
2021-04-17 23:42:51 +1200

    Update content

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <>
       <PageSeo
-        title={`About - ${siteMetadata.author}`}
+        title="About"
         description={`About me - ${siteMetadata.author}`}
         url={`${siteMetadata.siteUrl}/about`}
       />
@@ -35,12 +35,12 @@ export default function About() {
             </div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
-            <Image
+            {/* <Image
               src="/static/images/me.jpg"
               alt="Picture of the author"
               width={727}
               height={585}
-            />
+            /> */}
             <p>
               I'm a Frontend Engineer with 8+ years in the industry. I work with modern frontend
               technologies such as React, Redux, Node.js and TypeScript.
@@ -51,6 +51,30 @@ export default function About() {
               today's websites has changed dramatically allowing us to create seamless experiences
               and solve problems in ways deemed impossible just a few years ago.
             </p>
+            <h3>Career</h3>
+            <ul className="list-disc">
+              <li>
+                <em>Jan 2021 - Present:</em> Frontend Engineer Lead @ Parrot Analytics
+              </li>
+              <li>
+                <em>Jan 2019 - Jan 2021:</em> Senior Frontend Developer + People Lead @ Xero
+              </li>
+              <li>
+                <em>Feb 2018 - Jan 2019:</em> Senior Frontend Developer @ Xero
+              </li>
+              <li>
+                <em>Oct 2015 - Jan 2018:</em> Frontend Developer @ Xero
+              </li>
+              <li>
+                <em>Sept 2014 - Aug 2015:</em> Graduate Developer @ Xero
+              </li>
+              <li>
+                <em>Jun 2013 - Aug 2014:</em> Interaction Developer @ Infinity - Brand, Digital and
+                Strategy
+              </li>
+            </ul>
+            <h3>Location</h3>
+            <p>I currently live in the beautiful city of Auckland, New Zealand</p>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -16,19 +16,18 @@ export async function getStaticProps() {
 export default function Home({ posts }) {
   return (
     <>
-      <PageSeo
-        title={siteMetadata.title}
-        description={siteMetadata.description}
-        url={siteMetadata.siteUrl}
-      />
+      <PageSeo title="Home" description={siteMetadata.description} url={siteMetadata.siteUrl} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Hey{' '}
+            <span role="img" aria-label="waving hand">
+              👋🏼
+            </span>
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
+          <h2 className="text-2xl leading-7 text-gray-500 dark:text-gray-400">
+            I'm a Frontend Engineer Lead, technical coach and technology enthusiast.
+          </h2>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
